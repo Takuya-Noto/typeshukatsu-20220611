80f5864444e77462f481abbccdc07cb18fb6c7af
Takuya-Noto <63526038+Takuya-Noto@users.noreply.github.com>
2022-05-24 10:44:55 +0900

    Add files via upload

--- Q1_FizzBuzz.py
+++ Q1_FizzBuzz.py
@@ -2,15 +2,20 @@ for num in range(1, 101):
     string = ''
 
     # ここから記述
+
+    # 15の倍数であればFizzBuzz
     if num % 15 == 0:
         string = 'FizzBuzz'
 
+    # 15の倍数ではなくて、3の倍数であればFizz
     elif num % 3 == 0:
         string = 'Fizz'
 
+    # 15の倍数ではなくて、5の倍数であればBuzz
     elif num % 5 == 0:
         string = 'Buzz'
 
+    # 上記以外ならそのまま
     else:
         string = str(num)
 
